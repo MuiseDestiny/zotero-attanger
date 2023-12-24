b0aef098a333f2e4d75a8e1b7c5216b6b77f7f6b
Polygon <51939531+MuiseDestiny@users.noreply.github.com>
2023-12-24 22:23:53 +0800

    Initial commit

---
+++ addon/locale/en-US/preferences.ftl
@@ -0,0 +1,5 @@
+pref-title = Addon Template Example
+pref-enable =
+    .label = Enable
+pref-input = Input
+pref-help = { $name } Build { $version } { $time }
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { BasicTool } from "zotero-plugin-toolkit/dist/basic";
+import Addon from "./addon";
+import { config } from "../package.json";
+
+const basicTool = new BasicTool();
+
+if (!basicTool.getGlobal("Zotero")[config.addonInstance]) {
+  defineGlobal("window");
+  defineGlobal("document");
+  defineGlobal("ZoteroPane");
+  defineGlobal("Zotero_Tabs");
+  _globalThis.addon = new Addon();
+  defineGlobal("ztoolkit", () => {
+    return _globalThis.addon.data.ztoolkit;
+  });
+  Zotero[config.addonInstance] = addon;
+}
+
+function defineGlobal(name: Parameters<BasicTool["getGlobal"]>[0]): void;
+function defineGlobal(name: string, getter: () => any): void;
+function defineGlobal(name: string, getter?: () => any) {
+  Object.defineProperty(_globalThis, name, {
+    get() {
+      return getter ? getter() : basicTool.getGlobal(name);
+    },
+  });
+}
